2f55914c6bd00df90874182c35ee2c12efdb38f0
jjjjjin <22409515@student.uwa.edu.au>
2022-05-04 19:47:32 +0800

    add some pre-test code

--- app/app.py
+++ app/app.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/index')
+def index():
+    return '<h1>This is index page</h1>'
+    
+@app.route('/user_test/<name>')
+def user_test(name):
+    return render_template('user_test.html', name=name)
