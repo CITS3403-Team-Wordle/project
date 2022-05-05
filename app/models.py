f105cd68298708fbdf5410b9db2fd8dd2ede6663
jjjjjin <22409515@student.uwa.edu.au>
2022-05-05 16:41:01 +0800

    add dev_data.sqlite for database test

--- app/models.py
+++ app/models.py
@@ -1,2 +1,16 @@
-from flask_sqlalchemy import SQLalchemy
+from __init__ import db
+'''
+	TEST DEV DATABASE
+	TABLE: User
+	name varchar		passwd varchar		email varchar		uid ingeter
+'''
 
+class User(db.Model):
+	__tablename__ = 'User'
+	name = db.Column(db.String, unique = True)
+	passwd = db.Column(db.String)
+	email = db.Column(db.String)
+	uid = db.Column(db.Integer, primary_key = True)
+
+	def __ref__(self):
+		return 'User: {}\temail: {}'.format(self.name, self.email)
