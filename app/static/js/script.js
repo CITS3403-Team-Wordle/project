13d254291e1e2012d545b7b6d1cfbf01e753045a
chrisholloway1 <31472836+chrisholloway1@users.noreply.github.com>
2022-05-16 18:05:05 +0800

    current character hover and correct/incorrect typing of chars implemented

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,5 +1,6 @@
-// source = https://thepracticetest.com/typing/tests/practice-paragraphs/
+// ########### SETTING CONSTANTS ###########
 
+// source = https://thepracticetest.com/typing/tests/practice-paragraphs/
 const paragraphs = [
     "An ever-growing number of complex and rigid rules plus hard-to-cope-with regulations are now being legislated from state to state. Key federal regulations were formulated by the FDA, FTC, and the CPSC. Each of these federal agencies serves a specific mission. One example: Laws sponsored by the Office of the Fair Debt Collection Practices prevent an agency from purposefully harassing clients in serious debt. The Fair Packaging and Labeling Act makes certain that protection from misleading packaging of goods is guaranteed to each buyer of goods carried in small shops as well as in large supermarkets. Products on the market must reveal the names of all ingredients on the label. Language must be in clear and precise terms that can be understood by everyone. This practice is very crucial for the lives of many people. It is prudent that we recall that the FDA specifically requires that all goods are pure, safe, and wholesome. The FDA states that all goods be produced under highly sanitary conditions. Drugs must be completely safe and must also be effective for their stated purpose. This policy applies to cosmetics that must be both safe and pure. Individuals are often totally unappreciative of the FDA's great dedication.",
     "Editing is a growing field of work in the service industry. Paid editing services may be provided by specialized editing firms or by self-employed (freelance) editors. Editing firms may employ a team of in-house editors, rely on a network of individual contractors or both. Such firms are able to handle editing in a wide range of topics and genres, depending on the skills of individual editors. The services provided by these editors may be varied and can include proofreading, copy editing, online editing, developmental editing, editing for search engine optimization (SEO), etc. Self-employed editors work directly for clients or offer their services through editing firms, or both. They may specialize in a type of editing and in a particular subject area. Those who work directly for authors and develop professional relationships with them are called authors' editors.",
@@ -9,18 +10,51 @@ const paragraphs = [
     "One study examining 30 subjects, of varying different styles and expertise, has found minimal difference in typing speed between touch typists and self-taught hybrid typists. According to the study, \"The number of fingers does not determine typing speed... People using self-taught typing strategies were found to be as fast as trained typists... instead of the number of fingers, there are other factors that predict typing speed... fast typists... keep their hands fixed on one position, instead of moving them over the keyboard, and more consistently use the same finger to type a certain letter.\" To quote doctoral candidate Anna Feit: \"We were surprised to observe that people who took a typing course, performed at similar average speed and accuracy, as those that taught typing to themselves and only used 6 fingers on average\" (Wikipedia)",
     "Business meetings, and professional recordings can contain sensitive data, so security is something a transcription company should not overlook when providing services. Companies should therefore follow the various laws and industry best practice, especially so when serving law firms, government agencies or courts. Medical Transcription specifically is governed by HIPAA, which elaborates data security practices and compliance measures to be strictly followed, failure of which leads to legal action and penalties. Transcription security includes maintaining confidentiality of the data through information security practices including limiting access with passwords and ensuring a secure environment for data and appropriate methods of disposal of all materials and deletion of files. Personnel may be required to sign non-disclosure agreements on a regular basis as well as take various oaths regarding confidentiality and accuracy."
 ];
+const typingText = document.querySelector(".typing-text p"),
+inputField = document.querySelector(".wrap .input-field");
 
-const typingText = document.querySelector(".typing-text");
+let charIndex = 0;
 
 // ########### GAME LOGIC ###########
 
-function randomParagraph() {
-    console.log(paragraphs[0]);
+function generateRandomParagraph() {
+    // generate random number to get a random paragraph above
+    let randIndex = Math.floor(Math.random() * paragraphs.length);
+    // split all chars in random paragraph in to spans and add them inside <p>
+    paragraphs[randIndex].split("").forEach(span => {
+        let spanTag = `<span>${span}</span>`;
+        typingText.innerHTML += spanTag;
+    });
+    // when a key is pressed or mouse clicked focus on the input field so they
+    // can type and begin the test without clicking the space
+    document.addEventListener("keydown", () => inputField.focus());
+    typingText.addEventListener("click", () => inputField.focus());
 }
 
-randomParagraph();
+function  initiateTyping() {
+    const characters = typingText.querySelectorAll("span");
+    let typedChar = inputField.value.split("")[charIndex];
+    if (characters[charIndex].innerText === typedChar) {
+        characters[charIndex].classList.add("correct");
+    } else {
+        characters[charIndex].classList.add("incorrect");
+    }
+    charIndex += 1;
+    characters.forEach(span => span.classList.remove("active"));
+    characters[charIndex].classList.add("active");
+}
 
-console.log("hello");
+generateRandomParagraph();
+inputField.addEventListener("input", initiateTyping);
+
+
+
+
+
+
+
+
+
 
 // ########### AJAX  ###########
 
